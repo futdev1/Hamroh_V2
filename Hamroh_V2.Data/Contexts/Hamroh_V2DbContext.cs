cecb572ff249d3e70a5286a74ef3ed7295c1eb9f
abdusaminovilhomjon30037007@gmail.com <abdusaminovilhomjon30037007@gmail.com>
2022-11-12 16:14:41 +0500

    Add project files.

---
+++ Hamroh_V2.Data/Contexts/Hamroh_V2DbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamroh_V2.Data.Contexts
+{
+    public class Hamroh_V2DbContext : DbContext
+    {
+        public Hamroh_V2DbContext()
+        {
+
+        }
+    }
+}
---
+++ Hamroh_V2.Service/DTOs/ClientDTO/ClientForCreationDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamroh_V2.Service.DTOs.ClientDTO
+{
+    public class ClientForCreationDto
+    {
+        [Required]
+        public string Qayerdan { get; set; }
+
+        [Required]
+        public string Qayerga { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public int PeopleCount { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        public string Summa { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
