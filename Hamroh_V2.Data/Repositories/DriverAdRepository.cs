f02124727c0d40d776b360aebb779e299a4f8b3f
abdusaminovilhomjon30037007@gmail.com <abdusaminovilhomjon30037007@gmail.com>
2022-11-13 22:34:46 +0500

    Update

---
+++ Hamroh_V2.Data/Repositories/DriverAdRepository.cs
@@ -0,0 +1,18 @@
+﻿using Hamroh_V2.Data.Contexts;
+using Hamroh_V2.Data.IRepositories;
+using Hamroh_V2.Domain.Entities.DriverAds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamroh_V2.Data.Repositories
+{
+    public class DriverAdRepository : GenericRepository<DriverAd>, IDriverAdRepository
+    {
+        public DriverAdRepository(Hamroh_V2DbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
---
+++ Hamroh_V2.Data/Repositories/DriverRepository.cs
@@ -0,0 +1,20 @@
+﻿using Hamroh_V2.Data.Contexts;
+using Hamroh_V2.Data.IRepositories;
+using Hamroh_V2.Domain.Entities.Drivers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamroh_V2.Data.Repositories
+{
+    public class DriverRepository : GenericRepository<Driver>, IDriverRepository
+    {
+        public DriverRepository(Hamroh_V2DbContext dbContext)
+            : base(dbContext)
+        {
+
+        }
+    }
+}
